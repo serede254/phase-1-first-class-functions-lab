1575cd20a164b9e666219a6c7db028ce90e879f4
root <root@SURFACE.>
2024-03-20 07:34:38 +0300

    drivers

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// Code your solution in this file!
+const drivers=['Antonia','Nuru','Amari','Mo']
+const returnFirstTwoDrivers = function (drivers){
+return drivers.slice(0,2);
+}
+const returnLastTwoDrivers =function(drivers){
+    return drivers.slice(-2);
+}
+const selectingDrivers=[returnFirstTwoDrivers,returnLastTwoDrivers];
+const createFareMultiplier = function(mult){
+    return function(fare){
+        return mult*fare;
+    }
+}
+const fareDoubler = createFareMultiplier(2);
+const fareTripler = createFareMultiplier(3);
+
+function selectDifferentDrivers(drivers,func){
+    return func(drivers);
+}
